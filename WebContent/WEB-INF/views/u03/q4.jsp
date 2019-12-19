58f06389208c8d0cd6ddb0de20b1b540bb5c626a
sangle4@naver.com <sangle4@naver.com>
2019-12-19 17:47:29 +0900

    insert q4 function

--- WebContent/WEB-INF/views/u03/q4.jsp
+++ WebContent/WEB-INF/views/u03/q4.jsp
@@ -8,6 +8,7 @@
 <link rel="stylesheet" type="text/css" href="../../css/result_screen.css">
 <link href="https://fonts.googleapis.com/css?family=Nanum+Myeongjo|Ubuntu&display=swap" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="../../js/queryEvent.js"></script> <!-- 액션 js파일  -->
 <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -20,7 +21,7 @@
 			<div class = "user_status">
 				<form action="" method="POST" onsubmit = ""> 
 					<p>
-						고객ID&emsp;<input type="text" class="input_box" name="cust_id">
+						고객ID&emsp;<input onKeyup="this.value=this.value.replace(/[^0-9]/g,'');" type="text" class="input_box" name="cust_id">
 						<span style="float : right;">이름<input type="text" class="input_box" name="cust_name"></span>
 					</p>
 					<p>
@@ -47,25 +48,24 @@
 			<div class = "user_status">
 			<div style = "border-bottom : 1px solid #BEE0FF;">
 				<div class="tabfont" style = "width : 130px;font-size : 22px;">Customer ID</div>
-				<input name="userid" class="searchform" id = "idField">
-				<input type="submit" value="search" class="submitBT" onclick=""> 
+				<input onKeyup="this.value=this.value.replace(/[^0-9]/g,'');" name="userid" class="searchform" id = "idField">
+				<input type="submit" value="search" class="submitBT" onclick="q4func()"> 
 			</div>
-					<p>
-						고객ID&emsp;<input style = "background-color : #F2F8FF" type="text" class="input_box" name="cust_id" readonly="readonly">
-						<span style="float : right;">이름<input type="text" class="input_box" name="cust_name"></span>
-					</p>
-					<p>
-						로그인ID<input type="text" class="input_box" name="login_id">
-						<span style="float : right;">로그인PW<input type="text" class="input_box" name="login_pswd"></span>
-					</p>
-					<p>
-						접속별명<input type="text" class="input_box" name="login_name"> 
-						<span style="float : right;">성별<input type="text" class="input_box" name="cust_gender_type"></span>
-					</p>
-					<p>
-					고객등급<input type="text" class="input_box" name="cust_grade"> 
-					</p>
-				</form>
+				<p>
+					고객ID&emsp;<input style = "background-color : #F2F8FF" type="text" class="input_box" id="cust_id" readonly="readonly">
+					<span style="float : right;">이름<input type="text" class="input_box" id="cust_name"></span>
+				</p>
+				<p>
+					로그인ID<input type="text" class="input_box" id="login_id">
+					<span style="float : right;">로그인PW<input type="text" class="input_box" id="login_pswd"></span>
+				</p>
+				<p>
+					접속별명<input type="text" class="input_box" id="login_name"> 
+					<span style="float : right;">성별<input type="text" class="input_box" id="gender"></span>
+				</p>
+				<p>
+					고객등급<input type="text" class="input_box" id="grade"> 
+				</p>
 			</div>
 			<input type="submit" style="color : white; background-color : #1E60B5; margin : 0px 10px 10px 10px;" value="Delete" class="submitBT" onclick="">
 			<input type="submit" style="margin : 0px 10px 10px 10px;" value="Modify" class="submitBT" onclick="">
--- WebContent/js/queryEvent.js
+++ WebContent/js/queryEvent.js
@@ -1,9 +1,4 @@
 function q1func() {
-	if($('#idField').val() > 200000 || $('#idField').val() == "" || $('#idField').val() == 0){
-		alert("200000 이하 숫자만 가능합니다.");
-		$('#idField').val("");
-		return 0;
-	}
 	var input = $('#idField').val();
 	var data = new Object();
 	
@@ -22,8 +17,7 @@ function q1func() {
 		success : function(dat) {
 			var idx = dat.lastIndexOf('}');
 			var obj = JSON.parse(dat);
-			//alert("불러오기 성공!");
-			//alert(obj);
+			console.log(obj);
 			$("#cust_id").html(obj.cust_id);
 			$("#cust_name").html(obj.cust_name);
 			$("#login_id").html(obj.login_id);
@@ -35,73 +29,7 @@ function q1func() {
 	});
 }
 
-/*
 function q2func() {
-	if($('#idField').val() > 200000 || $('#idField').val() == "" || $('#idField').val() == 0){
-		alert("200000 이하 숫자만 가능합니다.");
-		$('#idField').val("");
-		return 0;
-	}
-	var input = $('#idField').val(); //입력 필드 데이터를 불러옴
-	if(index == 0) {//처음 호출 시 인덱스 그대로 입력
-		index = input;
-		$('#first').val(input); //검색 데이터 저장 공간에 저장
-	}
-	else
-		index = $('#index').val(); //호출한 내역이 존재 시 hidden input에 있는 데이터를 저장
-	
-	var data = new Object();
-	
-	if(input != $('#first').val()){ //검색한 데이터와 다른 데이터 검색 시
-		index = 0; //인덱스를 초기화하고 함수 다시 실행
-		q3func();
-	}
-	else{ //이어서 요청하는 경우
-		data.cust_id = index; //key로 똑같이 치환, input 넣을 때 사용
-		var data = $('#index').val();
-		
-		var jsonData = JSON.stringify(data);
-		
-		var urlText = "http://localhost:8080/com/intern/u03/tr2";
-		
-		$.ajax({
-			async : true,
-			type : "POST",
-			contentType : "application/json; charset=UTF-8",
-			url : urlText,
-			data : jsonData,
-			success : function(dat) {
-				var idx = dat.lastIndexOf('}');
-				var obj = JSON.parse(dat);
-				
-				var inner;
-				if(index == input) //첫 데이터 반환 시 html을 초기화
-					inner = "";
-				else
-					inner = tableBody.innerHTML; //데이터를 이어서 붙이는 경우 안의 데이터를 미리 받아서 저장
-				
-				for(var i = 0; i < obj.arr.length; i++){ //데이터를 덧붙여준다
-					inner += '<tr id="dbTR">';
-					inner += '<td>' + obj.arr[i].cust_id + '</td>';
-					inner += '<td>' + obj.arr[i].cust_name+ '</td>';
-					inner += '<td>' + obj.arr[i].login_id + '</td>';
-					inner += '<td>' + obj.arr[i].cust_grade + '</td></tr>';
-				}
-				tableBody.innerHTML = inner;
-				index = Number(index) + Number(obj.arr.length);
-				$("#index").val(index);
-			}
-		});
-	}
-}
-*/
-
-function q2func() {
-	if($('#idField').val() > 200000 || $('#idField').val() == ""){
-		alert("200000 이하 숫자만 가능합니다.");
-		$('#idField').val("");
-		return 0;
-	}
 	var input = $('#idField').val(); //입력 필드 데이터를 불러옴
 	if(Number(index) == 0) {//처음 호출 시 인덱스 그대로 입력
 		index = input;
@@ -134,6 +62,7 @@ function q2func() {
 			success : function(dat) {
 				var idx = dat.lastIndexOf('}');
 				var obj = JSON.parse(dat);
+				console.log(obj);
 				
 				var inner;
 				if(index == input) //첫 데이터 반환 시 html을 초기화
@@ -162,12 +91,42 @@ function q3func() {
 	q2func();
 }
 
-function q5func() {
+function q4func() {
 	if($('#idField').val() > 200000 || $('#idField').val() == "" || $('#idField').val() == 0){
 		alert("200000 이하 숫자만 가능합니다.");
 		$('#idField').val("");
 		return 0;
 	}
+	var input = $('#idField').val();
+	var data = new Object();
+	
+	data.cust_id = input; //key로 똑같이 치환, input 넣을 때 사용
+	
+	var jsonData = JSON.stringify(data);
+	
+	var urlText = "http://localhost:8080/com/intern/u03/tr1";
+	
+	$.ajax({
+		async : true,
+		type : "POST",
+		contentType : "application/json; charset=UTF-8",
+		url : urlText,
+		data : jsonData,
+		success : function(dat) {
+			var idx = dat.lastIndexOf('}');
+			var obj = JSON.parse(dat);
+			$("#cust_id").val(obj.cust_id);
+			$("#cust_name").val(obj.cust_name);
+			$("#login_id").val(obj.login_id);
+			$("#login_pswd").val(obj.login_pswd);
+			$("#login_name").val(obj.login_name);
+			$("#gender").val(obj.cust_gender_type);
+			$("#grade").val(obj.cust_grade);
+		}
+	});
+}
+
+function q5func() {
 	var input = $('#idField').val(); //입력 필드 데이터를 불러옴
 	if(index == 0) {//처음 호출 시 인덱스 그대로 입력
 		index = input;
@@ -252,9 +211,6 @@ function q6func() {
 		data.cust_id = index; //key로 똑같이 치환, input 넣을 때 사용
 		data.order_dt = order_dt;
 		
-		//alert("cust_id is " + index + " , order_dt is " + order_dt);
-		//return 0;
-		
 		var jsonData = JSON.stringify(data);
 		
 		var urlText = "http://localhost:8080/com/intern/u03/tr6";
@@ -278,7 +234,6 @@ function q6func() {
 				
 				for(var i = 0; i < obj.arr.length; i++){ //데이터를 덧붙여준다
 					inner += '<tr id="dbTR">';
-					alert(obj.arr[i].order_id);
 					inner += '<td>' + obj.arr[i].order_id + '</td>';
 					inner += '<td>' + obj.arr[i].order_dt + '</td>';
 					inner += '<td>' + obj.arr[i].order_channel_type + '</td>';
